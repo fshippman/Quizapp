f058f5e8456795f34bd29a6e4c6546422ce62389
fshippman <fshippman@yahoo.com>
2023-07-05 20:20:15 +0200

    detect which answer is klicked

--- script.js
+++ script.js
@@ -70,3 +70,19 @@ function showQuestion(){
     document.getElementById('answer_3').innerHTML = question['answer_3'];
     document.getElementById('answer_4').innerHTML = question['answer_4'];
 }
+
+function answer(selection){  // Die Funktion sagt ich brauch was um zu funktionieren! deshalb (selection)
+                            // auf den Karten ist ein onclick mit dem Text (string) der übergeben wird onclick="answer('answer_1')"
+                            // wenn onclick führe diese Funktion aus mit diesem Parameter der wird in die Funktion gesteckt      
+    let question = questions[curentQuestion]; // currentQuestion = 0  ----  questions[curentQuestion] = questions[0]
+    console.log('selected answer is', selection);
+    let selectedQuestionNumber = selection.slice(-1); // .slice(-1) nimmt das letzte Zeichen eines Worts, mit der Zeile wird erkannt welche Antwort gewählt wurde
+    console.log ('selectedQuestionNumber is' , selectedQuestionNumber)
+    console.log('current question is', question['question']);
+
+    if(selectedQuestionNumber == question['right_answer']) {
+        console.log('Richtige Antwort!');
+    } else {
+        console.log('Falsche Antwort!!!');
+    }
+}
